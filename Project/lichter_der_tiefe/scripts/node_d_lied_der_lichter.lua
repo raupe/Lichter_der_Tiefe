d5da8bf89f04b2093832763ed2982b8d32c8ee5b
Autarc <Autarc@gmail.com>
2012-12-29 03:26:06 +0100

    basis node fuer das 4.lied

---
+++ Project/lichter_der_tiefe/scripts/node_d_lied_der_lichter.lua
@@ -0,0 +1,17 @@
+-- declarations -> prevent globals
+if not v then v = {} end
+-- if not AQUARIA_VERSION then dofile("scripts/entities/entityinclude.lua") end
+
+
+-- on creation
+function init(me)
+
+	v.n = getNaija()
+end
+
+-- check trigger
+function activate(me)
+
+	learnSong(104)
+
+end
