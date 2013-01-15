e088fed9c1d5e25bea27817e5ec7feacba1e0e48
Julia <Julia@Cleo.(none)>
2013-01-15 10:54:51 +0100

    update darkness

--- Project/lichter_der_tiefe/scripts/darkness.lua
+++ Project/lichter_der_tiefe/scripts/darkness.lua
@@ -6,24 +6,22 @@ function init(me)
 	setupEntity(me)
 	entity_setEntityType(me, ET_NEUTRAL)
 	
+	v.flagLiedDerLichter = 1100
+	
 end
 
 -- after nodes have inited
 function postInit(me)
 
-	v.flagLiedDerLichter = 1100
-	
 	if getFlag(v.flagLiedDerLichter) == 1 then
-		v.dark = createQuad("lichtblume/lichtblume_darkness_2", 13)
-	else
 		v.dark = createQuad("lichtblume/lichtblume_darkness", 13)
-	end
-	
-	quad_setPosition(v.dark, entity_getPosition(me))
-	quad_scale(v.dark, 32, 32)
-	quad_alpha(v.dark, 0)
-	quad_alpha(v.dark, 1, 0.5)
-	quad_color(v.dark, 0, 0, 0)
+		quad_setPosition(v.dark, entity_getPosition(me))
+		quad_scale(v.dark, 32, 32)
+		quad_alpha(v.dark, 0)
+		quad_alpha(v.dark, 1, 0.5)
+		quad_color(v.dark, 0, 0, 0)
+	end	
+
 end
 
 function update(me, dt)	
