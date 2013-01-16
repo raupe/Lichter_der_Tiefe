c570c991a266e6e7c092d605959fc84c81fc9c8b
Julia <Julia@Cleo.(none)>
2013-01-16 16:24:30 +0100

    c_hoehle_der_entschlossenheit

--- Project/lichter_der_tiefe/scripts/node_c_hoehle_der_entschlossenheit.lua
+++ Project/lichter_der_tiefe/scripts/node_c_hoehle_der_entschlossenheit.lua
@@ -9,46 +9,15 @@ function init(me)
 	v.n = getNaija()
 	v.flag = 1000
 
-	-- centerText("Hoehle der Entschlossenheit")
-
-	v.nejl = 303
-	v.song = 1001
-	v.learn = false
-	v.songs = 307
-
-
-	if isFlag(v.nejl, 1) and isFlag(v.song, 0) then
-
-		node_setCursorActivation(me, true)
-
-		v.x,v.y = node_getPosition(me)
-		v.e = createEntity("c_lichterqualle", "", v.x, v.y)
-	end
+	centerText("Hoehle der Entschlossenheit")
 
 end
 
 -- trigger
 function activate(me)
-
-	if isFlag(v.song, 0) then
-
-		setFlag(v.song, 1)
-
-		entity_alpha(v.e, 0, 3)
-
-		learnSong(103)
-		setFlag(v.songs, getFlag(v.songs)+1 )
-		v.learn = true
-	end
 end
 
 -- check
 function update(me, dt)
-
-	if v.learn then
-
-		v.learn = false
-		setControlHint("Lichterqualle: Also gut, ich bringe dir den \"Klang der Entschlossenheit\" bei. Sei dir stest bewusst was du tust...", 0, 0, 0, 3)
-	end
 end
 
