ac65c9ed00365b55003e191768fe36d6b61fb25f
Julia <Julia@Cleo.(none)>
2013-01-29 17:16:04 +0100

    dunkelheit schwächer, aber größer + nur eine dunkelheit-entity in nejls höhle

--- Project/lichter_der_tiefe/scripts/darkness.lua
+++ Project/lichter_der_tiefe/scripts/darkness.lua
@@ -17,7 +17,7 @@ function postInit(me)
 	if getFlag(v.flagSongs) >= 3 then
 		v.dark = createQuad("lichtblume/lichtblume_darkness", 13)
 		quad_setPosition(v.dark, entity_getPosition(me))
-		quad_scale(v.dark, 32, 32)
+		quad_scale(v.dark, 64, 64)
 		quad_alpha(v.dark, 0)
 		quad_alpha(v.dark, 1, 0.5)
 		quad_color(v.dark, 0, 0, 0)
