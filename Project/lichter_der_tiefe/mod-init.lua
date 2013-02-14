eaa574227552334004deb971f6f731b7354f8f74
Julia <Julia@Cleo.(none)>
2013-02-14 23:02:34 +0100

    flags am anfang auf 0 setzen

--- Project/lichter_der_tiefe/mod-init.lua
+++ Project/lichter_der_tiefe/mod-init.lua
@@ -22,10 +22,84 @@ if not AQUARIA_VERSION then dofile("scripts/entities/entityinclude.lua") end
 
 function init()
 	
+	setFlag(1, 0)
+	setFlag(2, 0)
+	setFlag(3, 0)
+	setFlag(4, 0)
+	setFlag(5, 0)
+	setFlag(6, 0)
+	setFlag(7, 0)
+	
+	setFlag(100, 0)
+	setFlag(101, 0)
+	setFlag(102, 0)
+	setFlag(103, 0)
+	setFlag(104, 0)
+	setFlag(105, 0)
+	setFlag(106, 0)
+	setFlag(107, 0)
+	setFlag(108, 0)
+	
+	setFlag(200, 0)
+	setFlag(201, 0)
+	setFlag(202, 0)
+	setFlag(203, 0)
+	setFlag(204, 0)
+	
+	setFlag(300, 0)
+	setFlag(301, 0)
+	setFlag(302, 0)
+	setFlag(303, 0)
+	setFlag(304, 0)
+	setFlag(305, 0)
+	setFlag(306, 3)
+	setFlag(307, 0)
+	setFlag(308, 0)
+	setFlag(309, 0)
+	setFlag(310, 0)
+		
+	setFlag(400, 0)
+	setFlag(401, 0)
+	setFlag(402, 0)
+	setFlag(403, 0)
+	setFlag(404, 0)
+	setFlag(405, 0)
+	
+	setFlag(500, 0)
+	setFlag(501, 0)
+	setFlag(502, 0)
+	setFlag(503, 0)
+	setFlag(504, 0)
+	setFlag(505, 0)
+	setFlag(506, 0)
+	
+	setFlag(600, 0)
+	setFlag(601, 0)
+	setFlag(602, 0)
+	setFlag(603, 0)
+	setFlag(604, 0)
+	setFlag(605, 0)
+	setFlag(606, 0)
+	
+	setFlag(700, 0)
+	setFlag(701, 0)
+	setFlag(702, 0)
+	setFlag(703, 0)
+	setFlag(704, 0)
+	
+	setFlag(800, 0)
+	setFlag(801, 0)
+	setFlag(802, 0)
+	setFlag(803, 0)
+	
+	setFlag(900, 0)
 	setFlag(901, 0)
+	
+	setFlag(1000, 0)
+	setFlag(1001, 0)
+	
+	
 	setFlag(1100, 0)
-	setFlag(306, 3) -- path
-	setFlag(307, 0) -- songs
 	setFlag(1200, 0)
 	
 	loadMap("0_hafen")
